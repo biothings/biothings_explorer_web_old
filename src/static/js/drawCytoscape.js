8a1849ed640c786a36c55646c43d1a34ddac7435
kevinxin90 <kevinxin@scripps.edu>
2017-12-04 11:45:23 -0800

    add js function to draw cytoscape graph

---
+++ src/static/js/drawCytoscape.js
@@ -0,0 +1,16 @@
+function drawCytoscape(target_div, _style, _layout, data){
+	$("#cy").empty();
+	Plotly.purge('plotly-div');
+	$("#paths-list").empty();
+    $("#cy").show();
+    $("#plotly-div").hide();
+	var cy = cytoscape({
+		boxSelectionEnabled: false,
+        autounselectify: true,
+		container: $(target_div),
+		layout: _layout,
+		style: _style,
+		elements: data
+	});
+	return cy
+};
