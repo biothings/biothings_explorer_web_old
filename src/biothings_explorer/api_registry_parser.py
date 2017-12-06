223ae98e723cfcc5955866bea8822088c59bd36f
kevinxin90 <kevinxin@scripps.edu>
2017-12-05 17:28:47 -0800

    add verbose for prefix2uri function

--- src/biothings_explorer/api_registry_parser.py
+++ src/biothings_explorer/api_registry_parser.py
@@ -65,7 +65,7 @@ class RegistryParser:
                                                'preferred_name': row['Recommended name'], 'type': row['Type'], 'semantic type': row['Semantic Type']}
         return self.bioentity_info
 
-    def prefix2uri(self, prefix):
+    def prefix2uri(self, prefix, verbose=False):
         """
         Given a bio-entity in prefix format, return its URI
 
@@ -82,7 +82,8 @@ class RegistryParser:
             if v['preferred_name'] == prefix:
                 return k
         # print error message if no URI was found
-        print('No URI could be found for the prefix provided: {}'.format(prefix))
+        if verbose:
+            print('No URI could be found for the prefix provided: {}'.format(prefix))
 
     def read_api_list_file(self):
         """
