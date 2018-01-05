4cb95cce0e102ea2e9549ec2cad98b41d9f29dac
kevinxin90 <kevinxin@scripps.edu>
2018-01-04 22:41:22 -0800

    add color schema in Sankey

--- src/static/js/drawSankey.js
+++ src/static/js/drawSankey.js
@@ -1,3 +1,16 @@
+var datapoints = [
+                    {'type': 'ClinicalTrial', 'color': 'rgba(144, 144, 28, 0.4)'}, 
+                    {'type': 'Gene', 'color': 'rgba(55, 230, 84, 0.93)'},
+                    {'type': 'Drug', 'color': 'rgba(230, 55, 218, 0.93)'}, 
+                    {'type': 'Protein', 'color': 'rgba(55, 227, 230, 0.6)'},
+                    {'type': 'Allele/Variant', 'color': 'rgba(230, 174, 55, 0.83)'}, 
+                    {'type': 'ExperimentalStudy', 'color': 'rgba(86, 28, 144, 0.3)'},
+                    {'type': 'Phenotype', 'color': 'rgba(28, 86, 144, 0.3)'}, 
+                    {'type': 'Pathway', 'color': 'rgba(230, 55, 116, 0.63)'},
+                    {'type': 'Disease', 'color': 'rgba(166, 55, 230, 0.84)'}, 
+                    {'type': 'Reaction', 'color': 'rgba(100, 88, 77, 0.4)'}
+                ];
+
 function drawSankeyPlot(jsonResponse){
   var fig = jsonResponse;
   var data = {
@@ -46,4 +59,29 @@ function drawSankeyPlot(jsonResponse){
   Plotly.purge('plotly-div');
   $("#plotly-div").show();
   Plotly.plot('plotly-div', data, layout, {displayModeBar: false});
+  var svg = d3.select("#color-schema")
+             .append("svg")
+             .attr("width", 600)
+             .attr("height", 300);
+var rectangles = svg.selectAll('rect')
+                    .data(datapoints)
+                    .enter()
+                    .append('rect')
+                    .attr('x', 95)
+                    .attr('y', function(d, i) { return i * 30; })
+                    .attr('height', 20)
+                    .attr('width', 30)
+                    .style('fill', function(d) {
+                      return d['color']});
+
+var annotations = svg.selectAll('text')
+                    .data(datapoints)
+                    .enter()
+                    .append('text')
+                    .attr('x', 80)
+                    .attr('y', function(d, i) { return i * 30 + 15; })
+                    .text(function(d) { return d['type']; })
+                    .attr('font-size', 10)
+                    .attr('text-anchor', 'end');
+  $("#color-schema").show();
 };
