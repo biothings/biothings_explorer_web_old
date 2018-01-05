36e3ede60bb13de7787e6fea75841c45ecc61113
kevinxin90 <kevinxin@scripps.edu>
2018-01-04 22:43:21 -0800

    add hide schema function in draw cytoscape

--- src/static/js/drawCytoscape.js
+++ src/static/js/drawCytoscape.js
@@ -1,7 +1,28 @@
 function drawCytoscape(target_div, _style, _layout, data){
 	$("#cy").empty();
 	Plotly.purge('plotly-div');
-	$("#paths-list").empty();
+	$("#paths").hide();
+	//$("#log").hide();
+    $("#cy").show();
+    $("#plotly-div").hide();
+    $("#color-schema").hide();
+	var cy = cytoscape({
+		boxSelectionEnabled: false,
+        autounselectify: true,
+		container: $(target_div),
+		layout: _layout,
+		style: _style,
+		elements: data
+	});
+	//var nav = cy.navigator();
+	return cy;
+}
+
+function initializeCytoscape(target_div, _style, _layout) {
+	$("#cy").empty();
+	Plotly.purge('plotly-div');
+	$("#paths").hide();
+	//$("#log").hide();
     $("#cy").show();
     $("#plotly-div").hide();
 	var cy = cytoscape({
@@ -10,7 +31,6 @@ function drawCytoscape(target_div, _style, _layout, data){
 		container: $(target_div),
 		layout: _layout,
 		style: _style,
-		elements: data
 	});
 	//var nav = cy.navigator();
 	return cy;
