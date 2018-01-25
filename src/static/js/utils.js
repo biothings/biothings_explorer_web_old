6e14e47ba81b001adcd5d236871c49fc99a0fe0b
kevinxin90 <kevinxin@scripps.edu>
2018-01-25 14:01:11 -0800

    update download function

--- src/static/js/utils.js
+++ src/static/js/utils.js
@@ -17,16 +17,17 @@ function download(cy) {
 	})
     })
 }
-var COMMENT_LINE = "###############\n"
-var IMPORT_LINE = COMMENT_LINE + "#Import biothings_explorer python package\n" + COMMENT_LINE + "from biothings_explorer import BioThingsExplorer\n"
-var INITIATE_LINE = COMMENT_LINE + "#Start biothings_explorer instance\n" + COMMENT_LINE + "t = BioThingsExplorer()\n"
-var DEFINE_PATH_LINE = COMMENT_LINE + "#Here is the exploration path you select\n" + COMMENT_LINE + "PATH = path_list\n"
-var DEFINE_INPUT_LINE = COMMENT_LINE + "#Here is the input you give\n" + COMMENT_LINE + "INPUT = input_value\n"
-var EXPLORE_PATH_LINE = COMMENT_LINE + "#Here is the function to extract the output\n" + COMMENT_LINE + "output = t.find_output(t.path_conversion(PATH), INPUT, display_graph=False\n"
-var FINAL_COMMENT = "#If you are using jupyter notebook, set display_graph to True\n#This will display the graph on the jupyter notebook cell\n#You can do further data analysis using output.edges(), output.nodes()\n"
+var COMMENT_LINE = "<br>################################################<br>"
+var IMPORT_LINE = COMMENT_LINE + "<p>#Import biothings_explorer python package</p>" + COMMENT_LINE + "<p style='color:blue;'>from biothings_explorer import BioThingsExplorer</p>"
+var INITIATE_LINE = COMMENT_LINE + "#Start biothings_explorer instance" + COMMENT_LINE + "<p style='color:blue;'>t = BioThingsExplorer()</p>"
+var DEFINE_PATH_LINE = COMMENT_LINE + "#Your exploration starts from input_id to output_id" + COMMENT_LINE 
+var FIND_PATH_LINE = "<p style='color:blue;'>paths = t.find_path(start=input_id, end=output_id)</p>"
+var DEFINE_INPUT_LINE = COMMENT_LINE + "#Here is the input you give<br><br>" + COMMENT_LINE + "INPUT = input_value<br><br>"
+var EXPLORE_PATH_LINE = COMMENT_LINE + "#Here is the function to extract the output<br><br>" + COMMENT_LINE + "output = t.find_output(t.path_conversion(PATH), INPUT, display_graph=False<br><br>"
+var FINAL_COMMENT = "#If you are using jupyter notebook, set display_graph to True<br>#This will display the graph on the jupyter notebook cell<br><br>#You can do further data analysis using output.edges(), output.nodes()<br></p>"
 
-    function dynamic_text(path, _input) {
-	    var final_string = IMPORT_LINE + INITIATE_LINE + DEFINE_PATH_LINE.replace('path_list', JSON.stringify(path)) + DEFINE_INPUT_LINE.replace('input_value', _input) + EXPLORE_PATH_LINE + FINAL_COMMENT;
+    function dynamic_text(_input, _output) {
+	    var final_string = IMPORT_LINE + INITIATE_LINE + DEFINE_PATH_LINE.replace('input_id', _input).replace('output_id', _output) + FIND_PATH_LINE.replace('input_id', _input).replace('output_id', _output);
 	    console.log(final_string);
         return final_string;
     }
