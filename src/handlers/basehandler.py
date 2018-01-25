647f699dec6fad93542df10cfed4d1fe6fb66ae6
kevinxin90 <kevinxin@scripps.edu>
2018-01-25 10:38:10 -0800

    set header to be json

--- src/handlers/basehandler.py
+++ src/handlers/basehandler.py
@@ -5,3 +5,4 @@ class BaseHandler(tornado.web.RequestHandler):
         self.set_header("Access-Control-Allow-Origin", "*")
         self.set_header("Access-Control-Allow-Headers", "x-requested-with")
         self.set_header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS')
+        self.set_header('Content-Type', 'application/json')
