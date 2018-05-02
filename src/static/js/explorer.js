9697b275f0201fcb573d3b404ea31a7b51bb7b5d
kevinxin90 <kevinxin@scripps.edu>
2018-05-02 15:43:26 -0700

    start working on independent explorer model

---
+++ src/static/js/explorer.js
@@ -0,0 +1,16 @@
+$(function(){
+	$('.sidenav').sidenav();
+    $('.collapsible').collapsible();
+    //by default, when user opens the biothings explorer, 
+    //the overview of api road map will be displayed
+    drawSemanticMap();
+    drawIdLevelMap();
+    drawApiLevelMap();
+    drawColorSchema();
+    $(".tabs").tabs();
+    //populate the sidebar, fill in endpoint and bioentity names to 'select'
+    populateSelectInSideBar();
+    //when user select a bioentity/endpoint, display the sankey plot 
+    //related to the user request
+    displaySankeyBasedOnUserSelect();
+});
