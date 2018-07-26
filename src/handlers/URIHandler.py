0b4d7d1f77f50c3e456d336f0909ca2a54e9b84f
kevinxin90 <kevinxin@scripps.edu>
2018-07-26 13:37:09 -0700

    add check

--- src/handlers/URIHandler.py
+++ src/handlers/URIHandler.py
@@ -7,7 +7,9 @@ class BioThingsURIHandler(BaseHandler):
     def get(self, prefix):
         registry = RegistryParser(readmethod='filepath', initialize=True)
         uri = 'http://biothings.io/explorer/vocab/terms/' + prefix
+        print(uri)
         if uri in registry.bioentity_info:
+        	print('in registry')
             self.write(json.dumps(registry.bioentity_info[uri]))
         elif (uri + '/') in registry.bioentity_info:
             self.write(json.dumps(registry.bioentity_info[uri + '/']))
