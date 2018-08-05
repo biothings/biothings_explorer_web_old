6763d41852e9c76de80612b2af156ee324746631
kevinxin90 <kevinxin@scripps.edu>
2018-08-04 19:50:22 -0700

    handle back slash

--- src/handlers/URIHandler.py
+++ src/handlers/URIHandler.py
@@ -6,10 +6,12 @@ from .basehandler import BaseHandler
 class BioThingsURIHandler(BaseHandler):
     def get(self, prefix):
         registry = ApiCallHandler().registry
-        uri = 'http://biothings.io/explorer/vocab/terms/' + prefix
-        print(uri)
+        uri = 'http://biothings.io/explorer/vocab/terms/' + prefix.strip('/')
         if uri in registry.bioentity_info:
-            print('in registry')
             self.write(json.dumps(registry.bioentity_info[uri]))
         elif (uri + '/') in registry.bioentity_info:
             self.write(json.dumps(registry.bioentity_info[uri + '/']))
+        else:
+            self.set_status(400)
+            self.write(json.dumps({"status": 400, 'error message': "URI not found"}))
+            self.finish()
