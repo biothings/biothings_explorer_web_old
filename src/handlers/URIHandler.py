d81bf8bf51fabbbb1e2e309134fea2f26114907d
kevinxin90 <kevinxin@scripps.edu>
2018-08-10 10:42:35 -0700

    add slash

--- src/handlers/URIHandler.py
+++ src/handlers/URIHandler.py
@@ -1,16 +1,17 @@
 import json
+import tornado.web
+
 
 from biothings_explorer.api_call_handler import ApiCallHandler
 from .basehandler import BaseHandler
 
 class BioThingsURIHandler(BaseHandler):
+    @tornado.web.addslash
     def get(self, prefix):
         registry = ApiCallHandler().registry
-        uri = 'http://biothings.io/explorer/vocab/terms/' + prefix.strip('/')
+        uri = 'http://biothings.io/explorer/vocab/terms/' + prefix
         if uri in registry.bioentity_info:
             self.write(json.dumps(registry.bioentity_info[uri]))
-        elif (uri + '/') in registry.bioentity_info:
-            self.write(json.dumps(registry.bioentity_info[uri + '/']))
         else:
             self.set_status(400)
             self.write(json.dumps({"status": 400, 'error message': "URI not found"}))
