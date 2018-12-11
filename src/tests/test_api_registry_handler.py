598974b750bb16c8b66c21be5e5685c023b92406
sookieliao <kevinxin@scripps.edu>
2018-12-11 13:51:04 -0800

    flake8 fix

--- src/tests/test_api_registry_handler.py
+++ src/tests/test_api_registry_handler.py
@@ -4,14 +4,17 @@ from .context import BioThingsExplorer
 
 registry = BioThingsExplorer().registry
 
+
 class TestFunctionsInAPIRegistryParser(unittest.TestCase):
 
     def test_prefix2uri(self):
         """Test for prefix2uri function
         """
         # given a correct input, return correct result
-        self.assertEqual(registry.prefix2uri('ncbigene'), 'http://identifiers.org/ncbigene/')
-        self.assertEqual(registry.prefix2uri('hgnc.symbol'), 'http://identifiers.org/hgnc.symbol/')
+        self.assertEqual(registry.prefix2uri('ncbigene'),
+                         'http://identifiers.org/ncbigene/')
+        self.assertEqual(registry.prefix2uri('hgnc.symbol'),
+                         'http://identifiers.org/hgnc.symbol/')
         # given a invalid input, return None
         self.assertIsNone(registry.prefix2uri('nc'))
 
@@ -20,7 +23,8 @@ class TestFunctionsInAPIRegistryParser(unittest.TestCase):
         """
         # given a correct input, return correct result
         self.assertEqual(registry.prefix2semantictype('ncbigene'), 'gene')
-        self.assertEqual(registry.prefix2semantictype('chembl.compound'), 'chemical')
+        self.assertEqual(registry.prefix2semantictype('chembl.compound'),
+                         'chemical')
         self.assertEqual(registry.prefix2semantictype('mondo'), 'disease')
         # given an invalid input, return None
         self.assertIsNone(registry.prefix2semantictype('mm'))
@@ -31,11 +35,13 @@ class TestFunctionsInAPIRegistryParser(unittest.TestCase):
         # given a correct input, return correct result
         self.assertIn('ncbigene', list(registry.semantictype2prefix('gene')))
         self.assertIn('mondo', list(registry.semantictype2prefix('disease')))
-        self.assertNotIn('chembl.compound', list(registry.semantictype2prefix('anatomy')))
-        self.assertNotIn('chembl', list(registry.semantictype2prefix('anatomy')))
+        self.assertNotIn('chembl.compound',
+                         list(registry.semantictype2prefix('anatomy')))
+        self.assertNotIn('chembl',
+                         list(registry.semantictype2prefix('anatomy')))
         # given an invalid input, return None
         self.assertEqual(list(registry.semantictype2prefix('dd')), [])
 
+
 if __name__ == "__main__":
     unittest.main()
-
