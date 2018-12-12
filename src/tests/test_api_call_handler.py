7ec97099306d25d603b677e5d467f30d274f1aaa
sookieliao <kevinxin@scripps.edu>
2018-12-12 10:27:24 -0800

    add new tests for check multiple params

--- src/tests/test_api_call_handler.py
+++ src/tests/test_api_call_handler.py
@@ -8,15 +8,15 @@ ah = BioThingsExplorer().apiCallHandler
 class TestFunctionsInAPICallHandler(unittest.TestCase):
 
     def test_check_if_exists_multiple_params(self):
-        # test if raise keyerror when given an invalid endpoint name
-        #self.assertRaises(KeyError,
-                          #ah.check_if_exists_multiple_params(1))
-        #self.assertRaises(KeyError,
-                          #ah.check_if_exists_multiple_params('1'))
-        #self.assertRaises(KeyError,
-        #                  ah.check_if_exists_multiple_params('http://mygene.info/'))
+        # test invalid endpoint, 1 is integer
         with self.assertRaises(KeyError):
             ah.check_if_exists_multiple_params(1)
+        # test invalid endpoint, '1' is string
+        with self.assertRaises(KeyError):
+            ah.check_if_exists_multiple_params('1')
+        # test invalid endpoint, 'mygene.info' is API name, not endpoint
+        with self.assertRaises(KeyError):
+            ah.check_if_exists_multiple_params('http://mygene.info/')
         self.assertFalse(ah.check_if_exists_multiple_params('http://mygene.info/v3/querypathway'))
         self.assertFalse(ah.check_if_exists_multiple_params('http://mygene.info/v3/querygo'))
 
