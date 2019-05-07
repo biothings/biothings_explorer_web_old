006e92a149fc92dbf008aa0295c42723cb097b0e
sookieliao <kevinxin@scripps.edu>
2019-05-07 08:36:11 -0700

    add router

--- src/static/js/utils.js
+++ src/static/js/utils.js
@@ -1,120 +1,148 @@
-function download(cy) {
-	$("#DownloadPNGButton").click(function(){
-        var a = document.createElement('a');
-        a.href = cy.png();
-        a.download = 'api_map.png';
-        a.click();
-    });
-    $("#DownloadJSONButton").click(function() {
-    	var dataStr = "data:text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(cy.json()));
-		var dlAnchorElem = document.getElementById('downloadAnchorElem');
-		dlAnchorElem.setAttribute("href",     dataStr     );
-		dlAnchorElem.setAttribute("download", "biothings_explorer_data.json");
-		dlAnchorElem.click();
-	$("#DownloadCodeButton").click(function() {
-		var path = ['ncbigene', 'http://mygene.info/v3/gene/{geneid}', 'hgnc.symbol']
-		download_file('my_file.py', dynamic_text(path, '1017'), 'text/plain');
-	})
-    })
-}
-var COMMENT_LINE = "################################################################\n"
-var REQUEST_LINE = "from __future__ import print_function\nimport requests\n"
-var COMMENT_CALL_BIOTHINGS = "#Make API calls to BioThings Explorer\n"
-var EXTRACT_JSON_LINE = "data = doc.json()\nprint(data)\n"
-var COMMENT_EXTRACT_JSON = "#Extract JSON output\n"
-var IMPORT_LINE = COMMENT_LINE + "<p>#Import biothings_explorer python package</p><p> You can download the python package using pip install biothings_explorer</p>" + COMMENT_LINE + "<p style='color:blue;'>from biothings_explorer import BioThingsExplorer</p>"
-var INITIATE_LINE = COMMENT_LINE + "#Start biothings_explorer instance" + COMMENT_LINE + "<p style='color:blue;'>t = BioThingsExplorer()</p>"
-var DEFINE_PATH_LINE = COMMENT_LINE + "#Your exploration starts from input_id to output_id" + COMMENT_LINE 
-var FIND_PATH_LINE = "<p style='color:blue;'>paths = t.find_path(start=input_id, end=output_id)</p>"
-var DEFINE_INPUT_LINE = COMMENT_LINE + "#Here is the input you give<br><br>" + COMMENT_LINE + "INPUT = input_value<br><br>"
-var EXPLORE_PATH_LINE = COMMENT_LINE + "#Here is the function to extract the output<br><br>" + COMMENT_LINE + "output = t.find_output(t.path_conversion(PATH), INPUT, display_graph=False<br><br>"
-var FINAL_COMMENT = "#If you are using jupyter notebook, set display_graph to True<br>#This will display the graph on the jupyter notebook cell<br><br>#You can do further data analysis using output.edges(), output.nodes()<br></p>"
+import os
+import sys
+from os import path
 
-function construct_input_text(_input) {
-    var final_string = COMMENT_LINE + '#This code is for finding endpoints which take input "{input}"\n'.replace('{input}', _input) + COMMENT_LINE + REQUEST_LINE + COMMENT_CALL_BIOTHINGS
-    var query = "doc = requests.get('http://biothings.io/explorer/api/v2/input', params={'input': '{input}'})\n"
-    final_string += query.replace('{input}', _input)
-    final_string += COMMENT_EXTRACT_JSON
-    final_string += EXTRACT_JSON_LINE
-    return final_string
-};
+lib_path = os.path.abspath(os.path.join('..', 'biothings_explorer'))
+sys.path.append(path.dirname(path.dirname(path.abspath(lib_path))))
+from biothings_explorer import BioThingsExplorer
 
-function construct_output_text(_output) {
-    var final_string = COMMENT_LINE + '#This code is for finding endpoints which have output "{output}"\n'.replace('{output}', _output) + COMMENT_LINE + REQUEST_LINE + COMMENT_CALL_BIOTHINGS
-    var query = "doc = requests.get('http://biothings.io/explorer/api/v2/output', params={'output': '{output}'})\n"
-    final_string += query.replace('{output}', _output)
-    final_string += COMMENT_EXTRACT_JSON
-    final_string += EXTRACT_JSON_LINE
-    return final_string
-};
 
-function construct_endpoint_text(_endpoint) {
-    var final_string = COMMENT_LINE + '#This code is for finding the inputs and outputs of endpoint "{endpoint}"\n'.replace('{endpoint}', _endpoint) + COMMENT_LINE + REQUEST_LINE + COMMENT_CALL_BIOTHINGS
-    var query = "doc = requests.get('http://biothings.io/explorer/api/v2/endpoint', params={'endpoint': '{endpoint}'})\n"
-    final_string += query.replace('{endpoint}', _endpoint)
-    final_string += COMMENT_EXTRACT_JSON
-    final_string += EXTRACT_JSON_LINE
-    return final_string
-};
+color_dict = {
+              'gene': 'rgba(55, 230, 84, 0.93)',
+              'chemical': 'rgba(230, 55, 218, 0.93)', 
+              'protein': 'rgba(55, 227, 230, 0.6)',
+              'variant': 'rgba(230, 174, 55, 0.83)', 
+              'anatomy': 'rgba(86, 28, 144, 0.3)',
+              'phenotype': 'rgba(28, 86, 144, 0.3)', 
+              'pathway': 'rgba(230, 55, 116, 0.63)',
+              'disease': 'rgba(166, 55, 230, 0.84)', 
+              'transcript': 'rgba(100, 88, 77, 0.4)',
+              'organism': 'rgba(10, 133, 177, 0.4)',
+              'structure': 'rgba(8, 233, 7, 0.4)', 
+              'ontology': 'rgba(99,123,4,0.4)',
+              'bioassay': "rgba(100, 100, 100, 0.3)"}
 
-function construct_semantic_connect_text(_input, _output) {
-    var final_string = COMMENT_LINE + '#This code is for finding the endpoints which can connect from input "{input}" to output "{output}"\n'.replace('{input}', _input).replace('{output}', _output) + COMMENT_LINE + REQUEST_LINE + COMMENT_CALL_BIOTHINGS
-    var query = "doc = requests.get('http://biothings.io/explorer/api/v2/connectsemantictype', params={'input': '{input}', 'output': '{output}'})\n"
-    final_string += query.replace('{input}', _input).replace('{output}', _output)
-    final_string += COMMENT_EXTRACT_JSON
-    final_string += EXTRACT_JSON_LINE
-    return final_string
-};
+class HandlerUtils:
+    def __init__(self):
+        self.bt_explorer = BioThingsExplorer()
 
-function construct_id_connect_text(_input, _output, max_api) {
-    var final_string = COMMENT_LINE + '#This code is for finding the endpoints which can connect from input "{input}" to output "{output}"\n'.replace('{input}', _input).replace('{output}', _output) + COMMENT_LINE + REQUEST_LINE + COMMENT_CALL_BIOTHINGS
-    var query = "doc = requests.get('http://biothings.io/explorer/api/v2/findpath', params={'input': '{input}', 'output': '{output}', 'max_api': '{max_api}''})\n"
-    final_string += query.replace('{input}', _input).replace('{output}', _output).replace('{max_api}', max_api)
-    final_string += COMMENT_EXTRACT_JSON
-    final_string += EXTRACT_JSON_LINE
-    return final_string
-};
+    def find_edge_label(self, G, source, target, relation=None):
+        """
+        Given a MultiDiGraph, together with a source, target pair
+        Return the edge label info associated with the (source, target) pair
+        1) If only one label exists, return the label
+        2) When multiple label exists, if relation parameter is in the label(s), return the relation parameter
+        3) If relation parameter not in the labels, return None
 
-function construct_directinput2output_text(_input, _value, _output) {
-    var final_string = COMMENT_LINE + '#This code is for finding the output "{output}" which can be connected from {input}:{input_value}\n'.replace('{input}', _input).replace('{output}', _output).replace('{input_value', _value) + COMMENT_LINE + REQUEST_LINE + COMMENT_CALL_BIOTHINGS
-    var query = "doc = requests.get('http://biothings.io/explorer/api/v2/directinput2output', params={'input_prefix': '{input}', 'output_prefix': '{output}', 'input_value': '{input_value}'})\n"
-    final_string += query.replace('{input}', _input).replace('{input}', _input).replace('{output}', _output).replace('{input_value}', _value)
-    final_string += COMMENT_EXTRACT_JSON
-    final_string += EXTRACT_JSON_LINE
-    return final_string
-};
+        Parmas
+        ======
+        G: (multiDiGraph)
+            a multiDiGraph containaing nodes, edges and labels
+        source: (multiDiGraph node)
+        target: (multiDiGraph node)
+        relation:
+            The label given by user, default is None
 
-function construct_semanticinput2output_text(_input, _value, _output) {
-    var final_string = COMMENT_LINE + '#This code is for finding the output "{output}" which can be connected from {input}:{input_value} through semantic aligning results from multiple APIs\n'.replace('{input}', _input).replace('{output}', _output).replace('{input_value', _value) + COMMENT_LINE + REQUEST_LINE + COMMENT_CALL_BIOTHINGS
-    var query = "doc = requests.get('http://biothings.io/explorer/api/v2/semanticquery', params={'input_prefix': '{input}', 'output_prefix': '{output}', 'input_value': '{input_value}'})\n"
-    final_string += query.replace('{input}', _input).replace('{input}', _input).replace('{output}', _output).replace('{input_value}', _value)
-    final_string += COMMENT_EXTRACT_JSON
-    final_string += EXTRACT_JSON_LINE
-    return final_string
-};
+        Return
+        ======
+            label info for the source target pair
+        """
+        if (source, target) not in G.edges():
+            print('The given pair source-target pair ({}, {}) is not in the graph!'.format(source, target))
+            return None
+        edge_labels = [v['label'] for k, v in G.get_edge_data(source, target).items()]
+        if len(edge_labels) == 1:
+            return edge_labels[0]
+        elif len(edge_labels) > 1 and not relation:
+            return edge_labels
+        elif len(edge_labels) > 1 and relation and relation in edge_labels:
+            return relation
+        else:
+            return None
 
-    function dynamic_text(_input, _output) {
-	    var final_string = IMPORT_LINE + INITIATE_LINE + DEFINE_PATH_LINE.replace('input_id', _input).replace('output_id', _output) + FIND_PATH_LINE.replace('input_id', _input).replace('output_id', _output);
-	    console.log(final_string);
-        return final_string;
-    }
+    def label2color(self, label):
+        uri = self.bt_explorer.registry.prefix2uri(label)
+        if uri:
+            return color_dict[self.bt_explorer.registry.bioentity_info[uri]['semantic type']]
+        else:
+            return "rgba(250, 0, 0, 1.0)"
 
-    function download_file(name, contents, mime_type) {
-        mime_type = mime_type || "text/plain";
+    ###########################################################################
+    # Sample Input: (
+    #                 [('ncbigene', 'http://mygene.info/v1/'),
+    #                  ('ncbigene', 'http://myvariant.info/v1/'),
+    #                  ('http://mygene.info/v1/', 'hgnc.symbol'),
+    #                  ('http://myvariant.info/v1/', 'hgnc.symbol')])
+    # The input is the edges returned from networkx
+    # We need to take the input and feed it into plotly sankey plot
+    # The output which plotly sankey plot accepts looks like this:
+    # Sample Output:
+    # {
+    #    "label": ["ncbigene", "MyGene.info/v1/query",
+    #             "MyVariant.info/v1/query", "hgnc.symbol"],
+    #   "source": [0, 0, 1, 2], # represent the index in label
+    #    "target": [1, 2, 3, 3],
+    #    "value": [1,1,1,1] # edge weight, this doesn't apply for our use case
+    # }
+    # Issue: plotly fails to work if there are too many nodes
+    ###########################################################################
 
-        var blob = new Blob([contents], {type: mime_type});
+    def networkx_to_plotly(self, edges, duplicates_not_allowed=[]):
+        # initialize the output json doc
+        output_json = {'labels': [], 'colors': [], 'source': [], 'target': [], 'value': [], 'edge_labels': []}
+        # loop through each edge, load the first element into source
+        # and load the second element into target
+        # load all unique elements to the nodes
+        idx = 0
+        input_idx = {}
+        output_idx = {}
+        for _edge in edges:
+            if _edge[0] in duplicates_not_allowed:
+                if _edge[0] not in output_json['labels']:
+                    input_idx[_edge[0]] = idx
+                    output_idx[_edge[0]] = idx
+                    idx += 1
+                    output_json['labels'].append(_edge[0])
+                    output_json['colors'].append(self.label2color(_edge[0]))
+            elif _edge[0] not in input_idx:
+                input_idx[_edge[0]] = idx
+                idx += 1
+                output_json['labels'].append(_edge[0])
+                output_json['colors'].append(self.label2color(_edge[0]))
+            output_json['source'].append(input_idx[_edge[0]])
+            if _edge[1] in duplicates_not_allowed:
+                if _edge[1] not in output_json['labels']:
+                    input_idx[_edge[1]] = idx
+                    output_idx[_edge[1]] = idx
+                    idx += 1
+                    output_json['labels'].append(_edge[1])
+                    output_json['colors'].append(self.label2color(_edge[1]))
+            elif _edge[1] not in output_idx:
+                output_idx[_edge[1]] = idx
+                idx += 1
+                output_json['labels'].append(_edge[1])
+                output_json['colors'].append(self.label2color(_edge[1]))
+            output_json['target'].append(output_idx[_edge[1]])
+            output_json['edge_labels'].append(_edge[2])
+            if type(_edge[2]) == list:
+                output_json['value'].append(1*len(_edge[2]))
+            else:
+                output_json['value'].append(1)
+        return output_json
 
-        var dlink = document.createElement('a');
-        dlink.download = name;
-        dlink.href = window.URL.createObjectURL(blob);
-        dlink.onclick = function(e) {
-            // revokeObjectURL needs a delay to work properly
-            var that = this;
-            setTimeout(function() {
-                window.URL.revokeObjectURL(that.href);
-            }, 1500);
-        };
-
-        dlink.click();
-        dlink.remove();
-    }
+    def construct_knowledge_map(self):
+        triples = []
+        for _endpoint, _endpoint_info in self.bt_explorer.registry.endpoint_info.items():
+            relation = _endpoint_info['relation']
+            inputs = _endpoint_info['input']
+            for _input in inputs:
+                _input_curie = self.bt_explorer.registry.bioentity_info[_input]['prefix']
+                _input_type = self.bt_explorer.registry.bioentity_info[_input]['semantic type']
+                for _output, _relation in relation.items():
+                    _output_curie = self.bt_explorer.registry.bioentity_info[_output]['prefix']
+                    _output_type = self.bt_explorer.registry.bioentity_info[_output]['semantic type']
+                    for _relate in _relation:
+                        triples.append({'subject': {'prefix': _input_curie, 'semantic_type': _input_type}, 
+                                       'object': {'prefix': _output_curie, 'semantic_type': _output_type}, 
+                                       'predicate': _relate.split(':')[-1], 'endpoint': _endpoint, 'api': _endpoint_info['api']})
+        return triples
